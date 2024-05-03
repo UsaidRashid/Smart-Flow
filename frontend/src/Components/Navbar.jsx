b51aa18edc25e967b20891cda89ec79711eb8513
AMISH-KHAN <khanamish8923@gmail.com>
2024-05-03 11:21:42 +0530

    frontend test

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+      <header className="bg-[#1F3139] bg-opacity-85 text-white shadow-lg hidden md:block">
+  <div className="container mx-auto flex items-center h-24">
+    <a href="" className="flex items-center justify-center">
+      <img className=" h-24" src="./public/assets/images/logo.png" alt="" />
+    </a>
+    <nav className="contents font-semibold text-base lg:text-lg">
+      <ul className="mx-auto flex items-center">
+      
+      </ul>
+    </nav>
+  </div>
+</header>
+    </>
+  )
+}
