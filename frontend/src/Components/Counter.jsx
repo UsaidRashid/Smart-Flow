b600f7a9a25c59b8f884b04c45ceba2b32ff4a38
UsaidRashid <rashidusaid2@gmail.com>
2024-05-04 10:11:06 +0530

    Pushing index.js

--- frontend/src/Components/Counter.jsx
+++ frontend/src/Components/Counter.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Counter = ({ remainingTime }) => {
 
-    return (
-      <div className="counter text-5xl">
-        {remainingTime > 0 ? `${remainingTime} sec` : ''} 
-      </div>
-    );
-  };
+  return (
+    <div className="counter text-5xl">
+      {remainingTime > 0 ? `${remainingTime} sec` : ''} 
+    </div>
+  );
+};
   
-  export default Counter;
+export default Counter;
